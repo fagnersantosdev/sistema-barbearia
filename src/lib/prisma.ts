24a631dcb0d52d6c52046e2344e9c83680e78ec1
fagnersantosdev <fagnerpraxedes2@gmail.com>
2026-06-15 06:37:07 -0300

    aletrações para corrigir o erro de criação do banco prisma. adicionando o adaptador do prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,18 @@
-// src/lib/prisma.ts
-    // src/lib/prisma.ts
-    import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
+// Pega a URL do seu arquivo .env
+const connectionString = process.env.DATABASE_URL
 
-    export const prisma = new PrismaClient({
-      log: ['query', 'error'], // Isso vai mostrar os comandos SQL no terminal sempre que uma rota rodar!
-         errorFormat: 'pretty', // Isso vai mostrar os erros de forma mais legível no terminal
-    });
+// Cria o pool de conexão nativo do PostgreSQL
+const pool = new Pool({ connectionString })
+
+// Passa o pool para o adaptador do Prisma
+const adapter = new PrismaPg(pool)
+
+// Inicia o Prisma usando o adaptador
+export const prisma = new PrismaClient({
+  adapter,
+  log: ['query', 'error'], // Mantemos o log para ver o SQL no terminal
+})
